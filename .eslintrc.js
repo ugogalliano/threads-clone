7b5b6658dc743fd77901341a140eb8ca4be88d07
ugogalliano <89770321+ugogalliano@users.noreply.github.com>
2024-05-10 11:22:51 +0200

    refactor: eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
     'no-console': 'error',
     'prefer-const': 'warn',
     'no-var': 'warn',
-    'no-unused-vars': 'warn',
     'object-shorthand': 'warn',
     'quote-props': ['warn', 'as-needed'],
     // TypeScript rules
